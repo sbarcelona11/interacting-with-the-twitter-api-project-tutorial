d14ad4f01c1945bc07bbd76246d364bb45aa27e0
Sebastian Barcelona <sbarcelona@gmail.com>
2022-06-17 23:42:12 +0000

    add solution to problem

--- src/app.py
+++ src/app.py
@@ -1,8 +1,69 @@
 import os
 from dotenv import load_dotenv
+import tweppy
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # load the .env file variables
 load_dotenv()
 
+# install library
+# pip install tweepy
 
 # your app code here
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+bearer_token = os.environ.get('BEARER_TOKEN')
+
+client = tweepy.Client(bearer_token=bearer_token, 
+                        consumer_key=consumer_key, 
+                        consumer_secret=consumer_secret, 
+                        access_token=access_token, 
+                        access_token_secret=access_token_secret, 
+                        return_type = requests.Response,
+                        wait_on_rate_limit=True)
+
+query = '#100daysofcode (pandas OR python) -is:retweet'
+
+# get max. 100 tweets
+tweets = client.search_recent_tweets(query=query, 
+                                    tweet_fields=['author_id','created_at','lang'],
+                                     max_results=100)
+
+# Save data as dictionary
+tweets_dict = tweets.json() 
+
+# Extract "data" value from dictionary
+tweets_data = tweets_dict['data'] 
+
+# Transform to pandas Dataframe
+df = pd.json_normalize(tweets_data) 
+
+# save df
+df.to_csv("coding-tweets.csv")
+
+def word_in_text(word, text):
+    return re.search(word.lower(), text.lower())
+
+# Initialize list to store tweet counts
+[pandas, python] = [0, 0]
+
+# Iterate through df, counting the number of tweets in which each(pandas and python) is mentioned.
+for index, row in df.iterrows():
+    pandas += word_in_text('pandas', row['text'])
+    python += word_in_text('python', row['text'])
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['pandas', 'python']
+
+# Plot the bar chart
+ax = sns.barplot(cd, [pandas, python])
+ax.set(ylabel="count")
+plt.show()
